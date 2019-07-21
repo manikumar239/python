ac684ba2dcce24f081ebdd0cc92407ec4a92ff75
manikumar239 <manikumar.potani2014@gmail.com>
2019-07-21 13:06:29 +0530

    python

---
+++ ipynb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+genre = ['pop', 'rock', 'jazz','sapna']
+
+# iterate over the list using index
+leng = len(genre)
+for i in range(leng-1, leng-3, -1):
+    print("I like", genre[i])
+
+
+# In[ ]:
+
+
+
+
